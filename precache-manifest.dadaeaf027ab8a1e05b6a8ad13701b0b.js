2534c3f3314658fe0394744f36a935fcbd9e813c
TChagua <tea.chagua@gmail.com>
2019-02-09 00:32:32 +0400

    Updates

---
+++ precache-manifest.dadaeaf027ab8a1e05b6a8ad13701b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94ad17a47887a2afabf0",
+    "url": "/Dopos-website/static/js/runtime~main.94ad17a4.js"
+  },
+  {
+    "revision": "ead0b5a5f7f1f579d3be",
+    "url": "/Dopos-website/static/js/main.ead0b5a5.chunk.js"
+  },
+  {
+    "revision": "757cbf70b4eac6943274",
+    "url": "/Dopos-website/static/js/1.757cbf70.chunk.js"
+  },
+  {
+    "revision": "ead0b5a5f7f1f579d3be",
+    "url": "/Dopos-website/static/css/main.166ae09e.chunk.css"
+  },
+  {
+    "revision": "94f33975b8aa2f55ea446b1d7baae3d4",
+    "url": "/Dopos-website/index.html"
+  }
+];
